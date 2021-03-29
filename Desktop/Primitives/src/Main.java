45a2f0c0e101f07199943353e9ecd504dd1e7f47
DavidNazaryan23 <davnazaryan17@gmail.com>
2021-03-29 18:21:27 +0400

    first java commit

---
+++ Desktop/Primitives/src/Main.java
@@ -0,0 +1,36 @@
+public class Main {
+
+
+    public static void main(String[] args) {
+        char someChar = 'a';
+        System.out.println(someChar);
+
+        String someString = "some string";
+        System.out.println(someString);
+
+        boolean someBoolean = true;
+        System.out.println(someBoolean);
+
+
+        float someFloat= 2.3f;
+        System.out.println(someFloat);
+
+        double someDouble = 123.312;
+        System.out.println(someDouble);
+
+
+        byte someByte = 1 ;
+        System.out.println(someByte);
+
+        short someShort = 100;
+        System.out.println(someShort);
+
+        int someInt = 1110000;
+        System.out.println(someInt);
+
+        long someLong = 122323123L;
+        System.out.println(someLong);
+
+
+    }
+}
